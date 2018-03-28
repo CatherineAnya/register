a7926883159f691c9142dd8252af64df687dd671
CatherineAnya <839067728@qq.com>
2018-03-28 19:49:07 +0800

    replace tab to space

--- js/ui.js
+++ js/ui.js
@@ -2,25 +2,25 @@
 // 头部搜索框交互：jquery插件开发方法
 // 1.通过 $.fn 向jquery添加新方法：uiSearch方法的定义
 $.fn.uiSearch = function () {
-	// 获取对象
-	var ui = $(this);
-	// 2.考虑如何交互
-	// 首先，点击.ui-search-selected就显示出下拉列表.ui-search-list
-	$('.ui-search-selected', ui).on('click', function () {
-		$('.ui-search-list').show();
-		return false;
-	});
-	// 其次，点击.ui-search-list中任意一项a就改变.ui-search-selected中文本内容
-	$('.ui-search-list a', ui).on('click', function () {
-		$('.ui-search-selected').text($(this).text());
-		// 并且隐藏下拉列表.ui-search-list
-		$('.ui-search-list').hide();
-		return false;
-	});
-	// 最后，当下拉列表显示时，点击任意地方，除.ui-search-selected外，下拉列表隐藏
-	$('body').on('click', function () {
-		$('.ui-search-list').hide();
-	});
+    // 获取对象
+    var ui = $(this);
+    // 2.考虑如何交互
+    // 首先，点击.ui-search-selected就显示出下拉列表.ui-search-list
+    $('.ui-search-selected', ui).on('click', function () {
+        $('.ui-search-list').show();
+        return false;
+    });
+    // 其次，点击.ui-search-list中任意一项a就改变.ui-search-selected中文本内容
+    $('.ui-search-list a', ui).on('click', function () {
+        $('.ui-search-selected').text($(this).text());
+        // 并且隐藏下拉列表.ui-search-list
+        $('.ui-search-list').hide();
+        return false;
+    });
+    // 最后，当下拉列表显示时，点击任意地方，除.ui-search-selected外，下拉列表隐藏
+    $('body').on('click', function () {
+        $('.ui-search-list').hide();
+    });
 };
 
 // 选项卡组件
@@ -32,7 +32,7 @@ $.fn.uiSearch = function () {
   * @param {string} content tab组件中内容区的所有item，必选
   */
 $.fn.uiTab = function (header, content) {
-	// 获取对象
+    // 获取对象
     var ui = $(this);
     var tabs = $(header, ui);
     var cons = $(content, ui);
@@ -61,7 +61,7 @@ $.fn.uiTab = function (header, content) {
   * 5.高级效果：无缝滚动
   */
 $.fn.uiSlider = function () {
-	// 获取对象
+    // 获取对象
     var ui = $(this);
     var wrap = $('.ui-slider-wrap', ui);
     var items = $('.ui-slider-wrap .item', ui);
@@ -75,9 +75,9 @@ $.fn.uiSlider = function () {
     var enableAuto = true;
     // 自定义具体操作：上翻页move_prev、下翻页move_next、圆点导航切换图片move_to、自动轮播图片auto_move
     wrap.on('move_prev', function () {
-    	current = current - 1;
+        current = current - 1;
         if (current < 0) {
-        	current = size - 1;
+            current = size - 1;
         }
         wrap.triggerHandler('move_to', current);
     });
@@ -85,7 +85,7 @@ $.fn.uiSlider = function () {
     wrap.on('move_next', function () {
         current = current + 1;
         if (current >= size) {
-        	current = 0;
+            current = 0;
         }
         wrap.triggerHandler('move_to', current);
     });
@@ -97,7 +97,7 @@ $.fn.uiSlider = function () {
 
     wrap.on('auto_move', function () {
         setInterval(function () {
-        	enableAuto && wrap.triggerHandler('move_next');
+            enableAuto && wrap.triggerHandler('move_next');
         }, 2000);
     });
     // 事件
@@ -110,17 +110,17 @@ $.fn.uiSlider = function () {
     });
 
     tips.on('click', function () {
-    	var index = $(this).index();
-    	wrap.triggerHandler('move_to', index);
+        var index = $(this).index();
+        wrap.triggerHandler('move_to', index);
     });
 
     // 设置自动滚动感应
     ui.on('mouseover', function () {
-    	enableAuto = false;
+        enableAuto = false;
     });
 
     ui.on('mouseout', function () {
-    	enableAuto = true;
+        enableAuto = true;
     });
 
     wrap.triggerHandler('auto_move');
@@ -129,7 +129,7 @@ $.fn.uiSlider = function () {
 // 页面脚本逻辑
 // 3.调用我们定义的uiSearch、uiTab、uiSlider方法
 $(function () {
-	$('.ui-search').uiSearch();
+    $('.ui-search').uiSearch();
 
     $('.content-tab').uiTab('.capition > .item', '.container > .item');
     $('.content-tab .container .item').uiTab('.item-capition > a', '.item-content > .content-wrap');
